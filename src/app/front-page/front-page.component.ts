637bcf19aaf531624ef08926c68113749d4d6abc
Armani Araujo <aaraujo3@my.yorku.ca>
2023-08-21 13:48:08 -0400

    Updated to get/set items to/from session

--- src/app/front-page/front-page.component.ts
+++ src/app/front-page/front-page.component.ts
@@ -1,21 +1,39 @@
 import { Component } from '@angular/core';
 
-let todo_list: string[] = ['list item 1', 'list item 2', 'list item 3']
+/**
+ * Phase 1
+ * 
+ * 1. Retrieve any lists from session
+ * 2. If null, return nothing
+ * 3. If not null, 
+ *        parse from JSON to list
+ *        return list
+ * 
+ * 
+ * 
+ * Phase 2
+ * 1. Create new input and save buttons
 
 
+
+*/
+
+
+let todo_list: string[] | null
+let temp = ['list item 1', 'list item 2', 'list item 3']
+sessionStorage.setItem('todo_list', JSON.stringify(temp))
+
+let retrievedJSON = sessionStorage.getItem('todo_list')
+if (retrievedJSON != null)
+  todo_list = JSON.parse(retrievedJSON)
 
 @Component({
   selector: 'front-page',
   templateUrl: './front-page.component.html',
   styleUrls: ['./front-page.component.css']
 
-  
-
 })
 
-
-
-
 export class FrontPageComponent {
-  todo_list = todo_list;
+  user_todo_list = todo_list;
 }
