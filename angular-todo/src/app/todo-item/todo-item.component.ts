7cf0bb9add7cde77b5a6bf770b881598bfca67ff
Armani Araujo <aaraujo3@my.yorku.ca>
2023-08-20 22:44:38 -0400

    Created todo list item component

---
+++ angular-todo/src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent {
+}
