0b19382ad710e249dc9a8d33e6d243d406a2d2ba
Armani Araujo <aaraujo3@my.yorku.ca>
2023-08-21 13:48:42 -0400

    Testing

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,9 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.css']
 })
+
+
 export class TodoItemComponent {
+
+  @Input() item = ''
+  @Input() index: number = -1
+  
+  
 }
