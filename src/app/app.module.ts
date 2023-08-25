48dbefd9043e7973fa32ae88f33571e85deee1b6
Armani Araujo <aaraujo3@my.yorku.ca>
2023-08-24 23:07:07 -0400

    Added Cookie service and FormsModule dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
 import { FrontPageComponent } from './front-page/front-page.component';
+import { AddItemInputComponent } from './add-item-input/add-item-input.component';
+
+import { FormsModule } from '@angular/forms';
+import {CookieService} from 'ngx-cookie-service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoItemComponent,
-    FrontPageComponent
+    FrontPageComponent,
+    AddItemInputComponent
   ],
   imports: [
+
+    FormsModule,
+
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CookieService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
