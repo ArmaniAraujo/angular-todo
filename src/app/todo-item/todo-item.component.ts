aeceb1c4ef9b7a8102e1697c2b1fa8128b132ac4
Armani Araujo <aaraujo3@my.yorku.ca>
2023-08-24 23:08:33 -0400

    Added delete item code

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,10 +1,16 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+
+
 @Component({
   selector: 'todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.css']
+
+
+
 })
+
 
 
 export class TodoItemComponent {
@@ -12,5 +18,26 @@ export class TodoItemComponent {
   @Input() item = ''
   @Input() index: number = -1
   
+  deleteItem(index: number) {
+
+    let todo_list: string[] | null = []
+    let retrievedJSON = sessionStorage.getItem('todo_list')
+    if (retrievedJSON != null)
+      todo_list = JSON.parse(retrievedJSON)
+      if (todo_list !== null) {
+        // alert((todo_list[index]))
+        todo_list.splice(index, 1)
+        
+        // Writing new array with removed element back to sessionStorage
+        sessionStorage.setItem('todo_list', JSON.stringify(todo_list))
+
+        // Reloading page to load updated items in
+        window.location.reload()
+      }
+  }
+
+  add_item(item: string) {
+
+  }
   
 }
