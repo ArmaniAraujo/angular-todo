ffbd209ceca5ecf99fcda5d86da2d8a7b928b48e
Armani Araujo <aaraujo3@my.yorku.ca>
2023-10-09 20:48:26 -0400

    Backend Spring Boot init commit

---
+++ backend/src/test/java/com/sleepy/angulartodo/AngularTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleepy.angulartodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
