0af6ca647c6fb51c8b61764d42e4d5f29dfbdded
Armani Araujo <aaraujo3@my.yorku.ca>
2024-01-05 00:44:56 -0500

    User model slight modification

--- backend/src/main/java/com/sleepy/todo/model/User.java
+++ backend/src/main/java/com/sleepy/todo/model/User.java
@@ -91,13 +91,21 @@
 
 package com.sleepy.todo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.io.Serializable;
+import jakarta.persistence.Id;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import java.io.Serializable;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Table(name="Users")
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -111,15 +119,15 @@ public class User implements Serializable {
         return id;
     }
 
-    public User() {}
+//    public User() {}
 
-    public User(Long id, String name, String username, String email, String userImage) {
-        this.id = id;
-        this.name = name;
-        this.username = username;
-        this.email = email;
-        this.userImage = userImage;
-    }
+//    public User(Long id, String name, String username, String email, String userImage) {
+//        this.id = id;
+//        this.name = name;
+//        this.username = username;
+//        this.email = email;
+//        this.userImage = userImage;
+//    }
 
     public void setId(Long id) {
         this.id = id;
