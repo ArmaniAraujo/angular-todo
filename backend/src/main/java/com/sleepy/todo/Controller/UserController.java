06f92fad7e437c36ab9629930480e5fa65105817
Armani Araujo <aaraujo3@my.yorku.ca>
2024-01-08 22:44:55 -0500

    Added CORS support

--- backend/src/main/java/com/sleepy/todo/Controller/UserController.java
+++ backend/src/main/java/com/sleepy/todo/Controller/UserController.java
@@ -3,19 +3,32 @@ package com.sleepy.todo.Controller;
 import com.sleepy.todo.Service.UserService;
 import com.sleepy.todo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
+@RequestMapping("api/todo") // I dont get this
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-@GetMapping("/user/{email}")
-public User findUserByEmail(@PathVariable String email) {
-    return userService.findUserByEmail(email);
-}
+    @GetMapping("/test")
+    public String testConnection() {
+        return "Test successful!";
+    }
+    @GetMapping("/user/{email}")
+    public User findUserByEmail(@PathVariable String email) {
+        return userService.findUserByEmail(email);
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping(value = "/messages")
+    public ResponseEntity<List<String>> messages() {
+        return ResponseEntity.ok(Arrays.asList("first","second"));
+    }
 
 }
