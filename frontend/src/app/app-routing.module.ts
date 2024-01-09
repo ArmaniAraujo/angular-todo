7afeae9f68aa3f0c9983bb147949af032e745ddd
Armani Araujo <aaraujo3@my.yorku.ca>
2024-01-08 22:47:09 -0500

    Added route for login page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FrontPageComponent } from './front-page/front-page.component';
+import {LoginPageComponent} from "./login-page/login-page.component";
 
 const routes: Routes = [
   { path: '', component: FrontPageComponent }, // This is the starting page
+  { path: 'login', component: LoginPageComponent }, // Login Pagee
 ];
 
 @NgModule({
