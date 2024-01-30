2e2b9d76537461c72544b768424b8095c6278e64
Armani Araujo <aaraujo3@my.yorku.ca>
2024-01-30 00:52:10 -0500

    THIS IS WHAT LETS REQUESTS THROUGH (EXCLUDE SECURITY AUTOCONFIGURATION.CLASS) KILL ME.
    Employers don't hate me. I usually comment well. This is legitimately hell ok.

--- backend/src/main/java/com/sleepy/todo/TodosApplication.java
+++ backend/src/main/java/com/sleepy/todo/TodosApplication.java
@@ -2,28 +2,17 @@ package com.sleepy.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class TodosApplication {
 
+
 	public static void main(String[] args) {
-		String salt = "f1nd1ngn3m0";
-		String password = "potato";
-		String attemptPassword = new BCryptPasswordEncoder().encode(password);
-		System.out.println(attemptPassword);
-
-		if (BCrypt.checkpw(password, "$2a$10$TzVsf2OLMrZj2XgtTXIRh.HMm0SUQi8cjqSGA8n1iDg/9qkr/pMeK"))
-			System.out.println("youre in!!!");
-		else
-			System.out.println("sorry...");
-
-
-
-
-
 		SpringApplication.run(TodosApplication.class, args);
 	}
 
 }
+
