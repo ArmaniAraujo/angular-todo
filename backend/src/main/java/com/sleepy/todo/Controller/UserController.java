0abecd269ae163b78942b80533835219c804199b
Armani Araujo <aaraujo3@my.yorku.ca>
2024-01-30 00:51:10 -0500

    Minor testing changes

--- backend/src/main/java/com/sleepy/todo/Controller/UserController.java
+++ backend/src/main/java/com/sleepy/todo/Controller/UserController.java
@@ -1,16 +1,11 @@
 package com.sleepy.todo.Controller;
 
 import com.sleepy.todo.Service.UserService;
-import com.sleepy.todo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.List;
-
 @RestController
-@RequestMapping("api/todo") // I dont get this
+@RequestMapping("/api/todo") // I dont get this
 public class UserController {
 
     @Autowired
@@ -20,15 +15,10 @@ public class UserController {
     public String testConnection() {
         return "Test successful!";
     }
-    @GetMapping("/user/{email}")
-    public User findUserByEmail(@PathVariable String email) {
-        return userService.findUserByEmail(email);
-    }
 
     @CrossOrigin(origins = "http://localhost:4200")
-    @GetMapping(value = "/messages")
-    public ResponseEntity<List<String>> messages() {
-        return ResponseEntity.ok(Arrays.asList("first","second"));
+    @GetMapping("/testfront")
+    public String testFront() {
+        return "Test frontend request successful!";
     }
-
 }
