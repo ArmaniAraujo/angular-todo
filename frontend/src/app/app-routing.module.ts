b851eec08958b37c2f3934d69f84e7762dd0f1f3
Armani Araujo <aaraujo3@my.yorku.ca>
2024-02-02 02:01:59 -0500

    Added routes for new main page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FrontPageComponent } from './front-page/front-page.component';
 import {LoginPageComponent} from "./login-page/login-page.component";
+import {FrontPage2Component} from "./front-page-2/front-page-2.component";
 
 const routes: Routes = [
-  { path: '', component: FrontPageComponent }, // This is the starting page
-  { path: 'login', component: LoginPageComponent }, // Login Pagee
+  // { path: '', component: FrontPageComponent }, // This is the starting page
+  { path: '', component: LoginPageComponent }, // Login Page
+  { path: 'login', component: LoginPageComponent }, // Login Page
+  { path: 'front', component: FrontPage2Component},
 ];
 
 @NgModule({
