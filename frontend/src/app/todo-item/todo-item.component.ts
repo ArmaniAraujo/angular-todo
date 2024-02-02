d82658b7150f7cc15a9cb15e256e3733206d447c
Armani Araujo <aaraujo3@my.yorku.ca>
2024-02-02 02:00:00 -0500

    Removed old code

--- frontend/src/app/todo-item/todo-item.component.ts
+++ frontend/src/app/todo-item/todo-item.component.ts
@@ -1,6 +1,5 @@
-import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
-
+import {Component, Input} from '@angular/core';
+import { NgModule } from '@angular/core';
 
 
 @Component({
@@ -8,35 +7,9 @@ import { CookieService } from 'ngx-cookie-service';
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.css']
 })
-
-
-
 export class TodoItemComponent {
+  @Input() index!: number;
 
-  cookieService = inject(CookieService)
 
-  @Input() item = ''
-  @Input() index: number = -1
-  
-  deleteItem(index: number) {
-
-    alert('Going into function')
-
-    let todo_list: string[] | null = []
-    let retrievedJSON = this.cookieService.get('todo_list')
-    if (retrievedJSON != null)
-      todo_list = JSON.parse(retrievedJSON)
-      if (todo_list !== null) {
-        todo_list.splice(index, 1)
-        
-        // Writing new array with removed element back to sessionStorage
-        this.cookieService.set('todo_list', JSON.stringify(todo_list))
-        alert(this.cookieService.get('todo_list'))
-
-        // Reloading page to load updated items in
-        window.location.reload()
-
-      }
-  }
 
 }
