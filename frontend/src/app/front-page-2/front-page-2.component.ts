4f7b615f00d945762081a688e4462bd5dc4bb511
Armani Araujo <aaraujo3@my.yorku.ca>
2024-02-03 00:57:23 -0500

    Logic to print get user todos (CLEAN UP NEEDED)

--- frontend/src/app/front-page-2/front-page-2.component.ts
+++ frontend/src/app/front-page-2/front-page-2.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import { NgModule } from '@angular/core';
 import {TodoItem} from "../todo-item.model";
+import axios from "axios";
 
 @Component({
   selector: 'front-page-2',
@@ -13,13 +14,7 @@ export class FrontPage2Component {
   repeatArray = new Array(this.todoCount);
 
 
-  fakeTodoArray: TodoItem[] = [
-    { id: 1, task: 'Buy groceries for next week' },
-    { id: 2, task: 'Renew car insurance' },
-    { id: 3, task: 'Sign up for an online course' },
-    { id: 4, task: 'Complete coding assignment' },
-    { id: 5, task: 'Exercise for 30 minutes' },
-  ];
+  todosArray: TodoItem[] = [];
 
   /**
    * Constructor redirects to Login page if there is no user item in session storage
@@ -28,8 +23,59 @@ export class FrontPage2Component {
   constructor (private router: Router) {
     if (!sessionStorage.getItem("user"))
       this.router.navigate(["login"])
-    else
+    else {
       console.log(sessionStorage.getItem("user"))
+      this.retrieveTodos()
+    }
+  }
+
+  async retrieveTodos() : Promise<any> {
+    // If user exists in sessionStorage, retrieve todos
+    if (sessionStorage.getItem("user")) {
+
+      const userData = sessionStorage.getItem("user")
+      if (userData) {
+        const user = JSON.parse(userData) as {
+          name: string;
+          username: string;
+          hashed_password: string;
+          salt: string;
+          email: string;
+          userImage: string | null;
+        };
+        console.log("we printing")
+        const response = await axios.get('http://localhost:8080/api/todo/get_user_todos', {
+          params: { user: user.username}
+        })
+          .then(response => {
+            console.log("Response: ", response.data)
+            sessionStorage.setItem("todos", JSON.stringify(response.data))
+          })
+          .catch(error => {
+            console.log("Error: ", error);
+          })
+
+        // Proceed to load todos into page
+        this.loadTodos()
+
+      }
+    }
+  }
+
+  loadTodos() {
+    const todosString = sessionStorage.getItem("todos")
+    if (todosString) {
+
+    this.todosArray = JSON.parse(todosString);
+    console.log(this.todosArray)
+
+
+
+
+
+
+
+    }
   }
 
 }
