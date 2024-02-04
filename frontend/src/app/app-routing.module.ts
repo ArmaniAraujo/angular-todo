15a5fe37da32456efb549dcba5ef7177997cc576
Armani Araujo <aaraujo3@my.yorku.ca>
2024-02-04 01:15:13 -0500

    Removed unused imports and paths

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginPageComponent} from "./login-page/login-page.component";
-import {FrontPage2Component} from "./front-page-2/front-page-2.component";
+import {FrontPageComponent} from "./front-page/front-page.component";
 
 const routes: Routes = [
   // { path: '', component: FrontPageComponent }, // This is the starting page
   { path: '', component: LoginPageComponent }, // Login Page
   { path: 'login', component: LoginPageComponent }, // Login Page
-  { path: 'front', component: FrontPage2Component},
+  { path: 'front', component: FrontPageComponent},
 ];
 
 @NgModule({
